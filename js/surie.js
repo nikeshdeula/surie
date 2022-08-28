84520872a46ef3a3733e0ca170033257baf94e79
nikeshDeula <nikeshdeula.21@gmail.com>
2022-08-28 09:51:26 +0545

    surie website

---
+++ js/surie.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // alert(5);
+
+    // $("#clickMe").click(function(){
+    //     $(".nav-item").fadeIn();
+    // });
+
+    // $("#show").click(function(){
+    //     $(".nav-item").fadeOut(function(){
+    //         console.log("hello world");
+    //     }) ;
+    // });
+
+    // $("#clickMe").click(function(){
+    //     $("p").show();
+    // });
+
+
+    $("#openBtn").click(function(){
+        $("").toggleClass('')
+    })
+
+}); 
